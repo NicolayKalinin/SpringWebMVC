1619eb42d44e8114a05536456e23c96d4779b29f
Nicolay Kalinin <nikolajk202@gmail.com>
2023-08-15 18:02:48 +0300

    Files PostControllers modified

--- Di-java/src/main/java/ru/netology/controller/PostController.java
+++ Di-java/src/main/java/ru/netology/controller/PostController.java
@@ -13,34 +13,32 @@ public class PostController {
   public static final String APPLICATION_JSON = "application/json";
   private final PostService service;
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
+  @RestController
+  @RequestMapping("/api/posts")
+  public class PostController {
+    private final PostService service;
 
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    public PostController(PostService service) {
+      this.service = service;
+    }
 
-  public void getById(long id, HttpServletResponse response) throws IOException, NotFoundException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.getById(id);
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    @GetMapping
+    public List<Post> all() {
+      return service.all();
+    }
 
-  public void save(Reader body, HttpServletResponse response) throws IOException, NotFoundException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+      return service.getById(id);
+    }
 
-  public void removeById(long id, HttpServletResponse response) throws IOException, NotFoundException {
-    service.removeById(id);
-    response.setStatus(HttpServletResponse.SC_OK);
+    @PostMapping
+    public Post save(@RequestBody Post post) {
+      return service.save(post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeById(long id) {
+      service.removeById(id);
+    }
   }
-}
